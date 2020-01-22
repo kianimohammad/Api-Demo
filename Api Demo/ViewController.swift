5eb01b7e738bd72037fc802d8c6664a2e372fe92
Mohammad Kiani <kianimohammad@outlook.com>
2020-01-22 02:18:50 -0500

    API Demo

--- Api Demo/ViewController.swift
+++ Api Demo/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cityTxt: UITextField!
+    @IBOutlet weak var weatherLbl: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func selectCity(_ sender: UIButton) {
+        let url = URL(string: "http://api.openweathermap.org/data/2.5/weather?q=\(cityTxt.text!.replacingOccurrences(of: " ", with: "%20"))&APPID=5822f53158fdf0f22ed4fad25842783b")!
+                
+                let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+                    if let error = error {
+                        print(error)
+                    } else {
+                        if let urlContent = data {
+                            do {
+                                let jsonResult = try JSONSerialization.jsonObject(with: urlContent, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary
+//                                print(jsonResult)
+        //                        print(jsonResult?["name"] as! String)
+                                if let description = ((jsonResult?["weather"] as? NSArray)?[0] as? NSDictionary)?["description"] as? String {
+                                    print(description)
+                                    DispatchQueue.main.async {
+                                        self.weatherLbl.text = description
+                                    }
+                                }
+                            } catch {
+                                print(error)
+                            } 
+                        }
+                    }
+                }
+                // start the task
+                task.resume()
+    }
 }
 
